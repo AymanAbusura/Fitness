07ad430d1391daabe77815194d508c3dad314a6a
Ayman Abusura <aymansalameh1996@hotmail.com>
2023-08-25 14:56:28 +0300

    Done with Detail component for Exercise Page

---
+++ src/components/ExerciseVideos.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+const ExerciseVideos = () => {
+  return (
+    <Box>ExerciseVideos</Box>
+  )
+}
+
+export default ExerciseVideos
